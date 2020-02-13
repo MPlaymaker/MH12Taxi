9649f26d6dfd225f49fbbe17e15dfa68b6570157
MPlaymaker <50900322+MPlaymaker@users.noreply.github.com>
2020-02-13 16:01:05 +0530

    Update ICabSelectionService.h

--- MH12Taxi/MH12Taxi/ICabSelectionService.h
+++ MH12Taxi/MH12Taxi/ICabSelectionService.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include "CabDatabase.h"
 
+class Location;
 class CabDatabase;
 
 typedef std::vector<std::string> CabTypes;
@@ -11,7 +12,7 @@ class ICabSelectionService
 {
 public:
     virtual ICab* SelectCab(std::string ctype) = 0;
-    virtual CabTypes GetCabTypes() = 0;
+    virtual CabTypes GetCabTypes(Location* loc) = 0;
 };
 
 class PuneriCabSelectionService : public ICabSelectionService
@@ -25,27 +26,19 @@ public:
         this->cabDB = new CabDatabase;
     }
 
-    CabTypes GetCabTypes()
+    CabTypes GetCabTypes(Location* loc)
     {
-        return { "Mini", "Prime", "Sedan" };
+        return cabDB->getCabTypes(loc);
     }
 
     ICab* SelectCab(std::string ctype)
     {
-        std::vector<ICab*>availableCabs = cabDB->getCabs(ctype);
+        std::vector<ICab*> availableCabs = cabDB->getCabs(ctype);
 
         //TODO: Processing on the basis of selection
         if (availableCabs.empty())
             return nullptr;
         else
-        {
-            availableCabs[0].setStatus("Allotted");
             return availableCabs[0];
-        }
-    }
-    
-    ~PuneriCabSelectionService()
-    {
-        delete carDB;
     }
 };
