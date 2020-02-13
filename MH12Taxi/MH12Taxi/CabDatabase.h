4eb81b92a87feebd71b15748ff4711bb709577e1
MPlaymaker <50900322+MPlaymaker@users.noreply.github.com>
2020-02-13 15:57:57 +0530

    Update CabDatabase.h

--- MH12Taxi/MH12Taxi/CabDatabase.h
+++ MH12Taxi/MH12Taxi/CabDatabase.h
@@ -3,6 +3,7 @@
 #include <string>
 #include "ICab.h"
 
+class Location;
 class CabDatabase
 {
 private:
@@ -32,5 +33,20 @@ public:
         }
 
         return specificCabs;
+    }
+    
+    std::vector<std::string> getCabTypes(Location* loc)
+    {
+        std::vector<std::string> types;
+
+        for (auto itr : allCabs)
+        {
+            if (std::find(types.begin(), types.end(), itr->getCabType()) != types.end())
+            {
+                types.push_back(itr->getCabType());
+            }
+        }
+
+        return types;
     }
 };
